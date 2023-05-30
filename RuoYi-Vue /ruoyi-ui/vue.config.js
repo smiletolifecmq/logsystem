5e47192fcbd41b9f18fb956b192724c4a2aa0b9c
陈明强 <377259560@qq.com>
2023-05-30 10:26:23 +0800

    调整样式，修改名称

--- RuoYi-Vue /ruoyi-ui/vue.config.js
+++ RuoYi-Vue /ruoyi-ui/vue.config.js
@@ -7,7 +7,7 @@ function resolve(dir) {
 
 const CompressionPlugin = require('compression-webpack-plugin')
 
-const name = process.env.VUE_APP_TITLE || '日志管理系统' // 网页标题
+const name = process.env.VUE_APP_TITLE || '福清分公司综合信息管理系统' // 网页标题
 
 const port = process.env.port || process.env.npm_config_port || 80 // 端口
 
