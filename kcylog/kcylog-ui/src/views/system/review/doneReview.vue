f489a2c565e857c36d63636d2924d4bc7f8e54f9
陈明强 <377259560@qq.com>
2023-10-19 10:05:02 +0800

    雇工审核已办审核新增负责人部门查询

--- kcylog/kcylog-ui/src/views/system/review/doneReview.vue
+++ kcylog/kcylog-ui/src/views/system/review/doneReview.vue
@@ -47,6 +47,32 @@
           </el-option>
         </el-select>
       </el-form-item>
+
+      <el-form-item label="部门" prop="deptId">
+        <el-cascader
+          v-model="queryParamsDeptId"
+          :options="deptOptions"
+          @change="handleChangeDept"
+          clearable
+        ></el-cascader>
+      </el-form-item>
+
+      <el-form-item label="负责人" prop="userId">
+        <el-select
+          v-model="queryParams.fzrUserId"
+          filterable
+          placeholder="请选择"
+        >
+          <el-option
+            v-for="item in restaurants"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          >
+          </el-option>
+        </el-select>
+      </el-form-item>
+
       <el-form-item>
         <el-button
           type="primary"
@@ -584,6 +610,8 @@ import {
 import userInfo from "@/store/modules/user";
 import elDragDialog from "@/api/components/el-drag";
 import { listEmployee } from "@/api/system/reviewEmployee";
+import { deptTreeSelect } from "@/api/system/log";
+import { listUser } from "@/api/system/user";
 
 export default {
   filters: {
@@ -604,6 +632,14 @@ export default {
   },
   data() {
     return {
+      queryUserParams: {
+        pageNum: 1,
+        pageSize: 9999,
+        deptId: null,
+      },
+      restaurants: [],
+      deptOptions: undefined,
+      queryParamsDeptId: [],
       activeNames: ["1"],
       employmentReasonOptions: [
         {
@@ -697,8 +733,31 @@ export default {
       this.showStartEdit = false;
     }
     this.getUpcomingList();
+    this.getDeptTree();
+    this.loadAllUsers();
   },
   methods: {
+    loadAllUsers() {
+      listUser(this.queryUserParams).then((response) => {
+        for (let i = 0; i < response.rows.length; i++) {
+          let userObject = {
+            value: "",
+            userId: 0,
+          };
+          userObject.label = response.rows[i].userName;
+          userObject.value = response.rows[i].userId;
+          this.restaurants.push(userObject);
+        }
+      });
+    },
+    handleChangeDept(value) {
+      this.queryParams.deptId = value[1];
+    },
+    getDeptTree() {
+      deptTreeSelect().then((response) => {
+        this.deptOptions = transformIdToValue(response.data);
+      });
+    },
     handleChange(val) {
       // console.log(val);
     },
@@ -799,6 +858,8 @@ export default {
     resetQuery() {
       this.statusVaule = "";
       this.dateRange = [];
+      this.queryParamsDeptId = [];
+      this.queryParams.fzrUserId = null;
       this.resetForm("queryForm");
       this.handleQuery();
     },
@@ -837,4 +898,21 @@ export default {
     },
   },
 };
+function transformIdToValue(obj) {
+  if (Array.isArray(obj)) {
+    return obj.map((item) => transformIdToValue(item));
+  } else if (typeof obj === "object" && obj !== null) {
+    const newObj = {};
+    for (let key in obj) {
+      if (key === "id") {
+        newObj.value = obj[key];
+      } else {
+        newObj[key] = transformIdToValue(obj[key]);
+      }
+    }
+    return newObj;
+  } else {
+    return obj;
+  }
+}
 </script>
