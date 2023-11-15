e1c8e95fbff009790ea9df594b9f1b2dde634cc0
陈明强 <377259560@qq.com>
2023-11-15 09:03:55 +0800

    新增pdf、人工智能

---
+++ kcylog/kcylog-ui/src/views/system/tool/pdf.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="app-container">
+    <iframe
+      style="width: 100%; height: 600px"
+      src="http://localhost:8181/?lang=zh_CN"
+    ></iframe>
+  </div>
+</template>
