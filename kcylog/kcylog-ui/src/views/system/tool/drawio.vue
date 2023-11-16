5891d4e55b79a0cc98f10e42c2bfe0a31eb518d6
陈明强 <377259560@qq.com>
2023-11-16 16:19:51 +0800

    工具集合

---
+++ kcylog/kcylog-ui/src/views/system/tool/drawio.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="app-container">
+    <iframe
+      style="width: 100%; height: 600px"
+      src="http://127.0.0.1:9292/?lang=zh&offline=1"
+    ></iframe>
+  </div>
+</template>
